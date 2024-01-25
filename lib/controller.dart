e449db69d0bcd5602e971530edb301781e5f78a1
Antor <antor.ahmed@tetonelectronics.com>
2024-01-25 10:27:35 +0600

    init

---
+++ lib/controller.dart
@@ -0,0 +1,96 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:bonsoir/bonsoir.dart';
+import 'package:get/get.dart';
+
+class Controller extends GetxController {
+  Socket? socket;
+  var isConnected = false.obs;
+  var imageBytes = <int>[].obs;
+  var services = <Map<String, dynamic>>[].obs;
+  List<int> packetBytes = [];
+  var fps = 0.obs;
+  var lastFrameTime = 0;
+
+  Future<void> mDNSScanner() async {
+    if (isConnected.value) {
+      disconnect();
+      return;
+    }
+
+    try {
+      String type = '_camera._tcp';
+      BonsoirDiscovery discovery = BonsoirDiscovery(type: type, printLogs: false);
+      await discovery.ready;
+
+      discovery.eventStream!.listen((event) {
+        if (event.type == BonsoirDiscoveryEventType.discoveryServiceFound) {
+          print('Service found : ${event.service?.toJson()}');
+          event.service!.resolve(discovery.serviceResolver);
+        } else if (event.type == BonsoirDiscoveryEventType.discoveryServiceResolved) {
+          print('Service resolved : ${event.service?.toJson()}');
+          services.addIf(event.service!.name.isNotEmpty && !services.any((key) => key['name'] == event.service!.name),
+              event.service!.toJson(prefix: ''));
+        } else if (event.type == BonsoirDiscoveryEventType.discoveryServiceLost) {
+          print('Service lost : ${event.service?.toJson()}');
+          services.removeWhere((key) => key['name'] == event.service!.name);
+        }
+      });
+
+      await discovery.start();
+    } catch (e) {
+      printError(info: "$e");
+    }
+  }
+
+  void disconnect() {
+    socket?.destroy();
+    isConnected.value = false;
+    Get.snackbar('info', 'ESP32 Cam disconnected.');
+  }
+
+  Future<void> connectToESP(String socketIP, int socketPort) async {
+    if (socketIP == '' || socketPort == 0) {
+      Get.snackbar('error', 'IP or port invalid!');
+      return;
+    }
+    try {
+      socket = await Socket.connect(socketIP, socketPort);
+      isConnected.value = true;
+      Get.snackbar('info', 'ESP32 Cam connected.');
+
+      socket?.listen(
+        (data) {
+          try {
+            final header = (data[0] << 16) | (data[1] << 8) | data[2];
+            final footer = (data[data.length - 2] << 8) | data[data.length - 1];
+            if (header == 0xFFD8FF) {
+              packetBytes.assignAll(data);
+            } else if (footer == 0xFFD9) {
+              packetBytes.addAll(data);
+              if (packetBytes.length < 5000) return;
+              imageBytes.assignAll(packetBytes);
+              int ct = DateTime.now().millisecondsSinceEpoch;
+              fps.value = 1000 ~/ (ct - lastFrameTime);
+              lastFrameTime = DateTime.now().millisecondsSinceEpoch;
+            } else {
+              packetBytes.addAll(data);
+            }
+          } catch (e) {
+            print('Packet error: $e');
+          }
+        },
+        onError: (error) {
+          print('Socket error: $error');
+          disconnect();
+        },
+        cancelOnError: true,
+      );
+    } catch (e) {
+      print('Error connecting to the server: $e');
+      Get.snackbar('error', 'Something wrong!');
+      isConnected.value = false;
+    }
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'controller.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final ctx = Get.put(Controller());
+
+    Widget appBody() {
+      if (ctx.isConnected.value) {
+        return Padding(
+          padding: const EdgeInsets.all(20),
+          child: Stack(
+            children: [
+              SizedBox(
+                width: context.mediaQuery.size.width,
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.all(Radius.circular(20)),
+                  child: ctx.imageBytes.isNotEmpty
+                      ? Image.memory(
+                          Uint8List.fromList(ctx.imageBytes),
+                          fit: BoxFit.fill,
+                          gaplessPlayback: true,
+                          filterQuality: FilterQuality.high,
+                        )
+                      : const Center(child: Text('Waiting for image frame...')),
+                ),
+              ),
+              Positioned(
+                top: 20,
+                left: 20,
+                child: Text(
+                  "FPS: ${ctx.fps.value}",
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                    color: Colors.deepOrange,
+                  ),
+                ),
+              )
+            ],
+          ),
+        );
+      }
+
+      return Obx(
+        () => ctx.services.isEmpty
+            ? const Center(child: Text('Scan for services...', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)))
+            : Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: ListView(
+                  children: ctx.services
+                      .map(
+                        (service) => Padding(
+                          padding: const EdgeInsets.only(bottom: 10),
+                          child: Card(
+                            child: ListTile(
+                              title: Text(service['name'] ?? '', style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+                              subtitle: Text(service['type'] ?? ''),
+                              trailing: Text("${service['host'] ?? ''}:${service['port'] ?? ''}",
+                                  style: const TextStyle(fontWeight: FontWeight.bold)),
+                              onTap: () => ctx.connectToESP(service['host'] ?? '', service['port'] ?? 0),
+                            ),
+                          ),
+                        ),
+                      )
+                      .toList(),
+                ),
+              ),
+      );
+    }
+
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Padding(
+            padding: const EdgeInsets.all(5),
+            child: Image.asset('assets/logo.png'),
+          ),
+          title: const Text('Safety Camera LIVE'),
+          actions: [
+            Obx(
+              () => ElevatedButton(
+                onPressed: () => ctx.mDNSScanner(),
+                child: Text(ctx.isConnected.value ? 'Disconnect' : 'Scan'),
+              ),
+            ),
+            const SizedBox(width: 10)
+          ],
+        ),
+        body: Obx(() => appBody()),
+      ),
+    );
+  }
+}
